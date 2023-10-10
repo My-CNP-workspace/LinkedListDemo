1d6910316c078873bfc83be72741d33f139bb13d
charithanayana <charithanayana@gmail.com>
2023-10-10 15:12:16 +0530

    features implimenting

--- src/LinkListDemo.java
+++ src/LinkListDemo.java
@@ -4,6 +4,13 @@ public class LinkListDemo {
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
 
+        ll.add(0,"charitha");
+        ll.add(1,"nayanajith");
+        ll.add(2,1987);
+        ll.add(3,"june");
+        ll.add(4,12);
+
+        System.out.println(ll);
 
     }
 }
